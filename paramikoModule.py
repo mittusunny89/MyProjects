c047b92a495840988592590ff095646e66220737
mittusunny89 <121948636+mittusunny89@users.noreply.github.com>
2023-08-15 08:09:18 -0400

    Add files via upload

---
+++ paramikoModule.py
@@ -0,0 +1,33 @@
+import paramiko
+
+# Create object of SSHClient and
+# connecting to SSH
+ssh = paramiko.SSHClient()
+
+# Adding new host key to the local
+# HostKeys object(in case of missing)
+# AutoAddPolicy for missing host key to be set before connection setup.
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+ssh.connect('localhost', port=22, username='ageorge',
+			password='bayshore1234$', timeout=3)
+
+# Execute command on SSH terminal
+# using exec_command
+#stdin, stdout, stderr = ssh.exec_command('show ip interface brief')
+
+stdin, stdout, stderr = ssh.exec_command('ls -l /home/mittoos')
+
+def find_files(mystring):
+	mystring = stdout.read().decode()
+	myitems = mystring.splitlines()
+	myfiles = []
+	for item in myitems:
+		if '-rw-r--r--' in item:
+			myfiles.append(item)
+	for file in myfiles:	
+		words = file.split()
+	#print(words)	
+		print(f'The file name is {words[-1]} with  size  {words[4]}')
+find_files(stdout)
+ssh.close()
